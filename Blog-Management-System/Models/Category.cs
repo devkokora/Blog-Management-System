826362354c9fa1481d5ef689ea0b9af3334146e7
devkokora <dev.kokora@gmail.com>
2024-05-04 17:51:32 +0700

    add v2 diagram

--- Blog-Management-System/Models/Category.cs
+++ Blog-Management-System/Models/Category.cs
@@ -5,7 +5,7 @@ namespace Blog_Management_System.Models
     public class Category
     {
         public int CategoryId { get; set; }
-        public string? Name { get; set; }
+        public string? CategoryName { get; set; }
         public List<Forum>? Forums { get; set; }
     }
 }
