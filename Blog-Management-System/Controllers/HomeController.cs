5c01cf842541876d575a7b7cfdd94c4bb2bac79a
devkokora <dev.kokora@gmail.com>
2024-05-04 13:36:47 +0700

    add pics dbdiagram

--- Blog-Management-System/Controllers/HomeController.cs
+++ Blog-Management-System/Controllers/HomeController.cs
@@ -18,11 +18,6 @@ namespace Blog_Management_System.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
