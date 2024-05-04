869170d382161e43c118549e0a026f5b93b82557
devkokora <dev.kokora@gmail.com>
2024-05-04 12:26:59 +0700

    Add project files.

---
+++ Blog-Management-System/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Blog_Management_System.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
